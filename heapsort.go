d43f41f1faef1df55ff4c45f23a8376c7f5b92f0
maksim <m.hryshyn@andersenlab.com>
2022-01-29 13:33:59 +0300

    heapsort

---
+++ heapsort.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := []int{4, 1, 3, 2, 16, 9, 10, 14, 8, 7}
+	heapSort(&a)
+	fmt.Println(a)
+}
+
+func left(i int) int { return 2 * i }
+
+func right(i int) int { return 2*i + 1 }
+
+// Complexity O(lg n)
+// needs for supporting non-increasing pyramid
+func maxHeapify(a *[]int, i int, heapSize int) {
+	l := left(i)
+	r := right(i)
+	var largest int
+	// we define which element is the largest left, right or parent
+	if l <= heapSize && (*a)[l-1] > (*a)[i-1] {
+		largest = l
+	} else {
+		largest = i
+	}
+	if r <= heapSize && (*a)[r-1] > (*a)[largest-1] {
+		largest = r
+	}
+	// if largest is left or right we swap values
+	if largest != i {
+		// complexity 0(1)
+		(*a)[i-1], (*a)[largest-1] = (*a)[largest-1], (*a)[i-1]
+		// to support property of non-increasing pyramid
+		maxHeapify(a, largest, heapSize)
+	}
+}
+
+// Complexity O(n) - linear time
+// building a non-increasing pyramid
+// Property of non-increasing pyramid is that the condition is fulfilled
+// for each node (except of root) with i index
+// A[parent(i)] >= A[i]
+func buildMaxHeap(a *[]int, length int) {
+	// we start building a pyramid from the tree leaves
+	// which each of them is pyramid with one element
+	for i := length / 2; i > 0; i-- {
+		maxHeapify(a, i, length)
+	}
+}
+
+// Complexity O(n log n)
+func heapSort(a *[]int) {
+	length := len(*a)
+	buildMaxHeap(a, length)
+	for i := length; i > 1; i-- {
+		// The largest element (A[0]) is in the top of the pyramid
+		// So we swap it with the smallest (A[n]) element
+		(*a)[0], (*a)[i-1] = (*a)[i-1], (*a)[0]
+		// we decrement heap size
+		length--
+		// recovery a property of non-increasing pyramid
+		maxHeapify(a, 1, length)
+	}
+}
