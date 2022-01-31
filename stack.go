618ead9de397241af5868aa709c4ea57a25bdfb4
maksim <m.hryshyn@andersenlab.com>
2022-01-31 11:06:58 +0300

    stack

---
+++ stack.go
@@ -0,0 +1,31 @@
+package main
+
+type Link struct {
+	key  int
+	prev *Link
+	next *Link
+}
+
+type LinkedList struct {
+	head   *Link
+	bucket []Link
+}
+
+func (ll *LinkedList) ListInsert(x int) {
+	newLink := Link{
+		key:  x,
+		next: ll.head,
+	}
+	if ll.head != nil {
+		ll.head.prev = &newLink
+	}
+	ll.head = &newLink
+	newLink.prev = nil
+	ll.bucket = append(ll.bucket, newLink)
+	return
+}
+
+func listDelete(ll *LinkedList) (x int) {
+	ll.bucket = append(ll.bucket[:x], ll.bucket[x+1:]...)
+	return
+}
