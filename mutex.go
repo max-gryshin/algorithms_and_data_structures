5a7ff9a17ee9bd5a6f9d3185155e13f1343aaf6a
maksim <m.hryshyn@andersenlab.com>
2022-03-28 17:04:42 +0300

    crud Adding dto.

---
+++ mutex.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	atomic2 "go.uber.org/atomic"
+)
+
+type PetersonMutex struct {
+	want    [2]atomic2.Bool
+	waiting atomic2.Int64
+}
+
+func (pm *PetersonMutex) Lock(threadId int64) {
+	other := 1 - threadId
+	pm.want[threadId].Store(true)
+	pm.waiting.Store(threadId)
+	for pm.want[other].Load() &&
+		pm.waiting.Load() == threadId {
+	}
+}
+
+func (pm *PetersonMutex) Unlock(threadId int) {
+	pm.want[threadId].Store(false)
+}
+
+func NewPetersonMutex() *PetersonMutex {
+	return &PetersonMutex{}
+}
