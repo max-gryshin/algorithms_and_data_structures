6b845e7bd53f587929dac05906066ebae3cbd8ae
maksim <m.hryshyn@andersenlab.com>
2022-07-06 14:30:09 +0300

    fix test.

--- data_structure/double_list_test.go
+++ data_structure/double_list_test.go
@@ -13,14 +13,12 @@ var doubleListTests = []struct {
 }
 
 var doubleListTestsCurrent = []struct {
-	n    []int
-	v    []int
-	list *DoubleList
+	n []int
+	v []int
 }{
 	{
 		[]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
 		[]int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1},
-		NewDoubleList(),
 	},
 }
 
