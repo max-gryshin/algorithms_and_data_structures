bc14552f5e43686ecca5df29908827f967adf3c1
max-gryshin <goooglemax1993@gmail.com>
2023-10-01 10:58:38 +0200

    Implement Priority queue.

--- algorythm/sort/heapsort.go
+++ algorythm/sort/heapsort.go
@@ -33,6 +33,31 @@ func MaxHeapifyRecursive(a []int, i int, heapSize int) {
 	}
 }
 
+// MinHeapifyRecursive Complexity O(lg n)
+// needs for supporting non-descending pyramid
+// Recursive approach
+func MinHeapifyRecursive(a []int, i int, heapSize int) {
+	l := left(i)
+	r := right(i)
+	var min int
+	// we define which element is the min left, right or parent
+	if l <= heapSize && a[l] < a[i] {
+		min = l
+	} else {
+		min = i
+	}
+	if r <= heapSize && a[r] < a[min] {
+		min = r
+	}
+	// if min is left or right we swap values
+	if min != i {
+		// complexity 0(1)
+		a[i], a[min] = a[min], a[i]
+		// to support property of non-increasing pyramid
+		MinHeapifyRecursive(a, min, heapSize)
+	}
+}
+
 // MaxHeapify Complexity O(lg n)
 // needs for supporting non-increasing pyramid
 func MaxHeapify(a []int, i int, heapSize int) {
@@ -41,18 +66,18 @@ func MaxHeapify(a []int, i int, heapSize int) {
 		l = left(k)
 		r = right(k)
 		// we define which element is the largest left, right or parent
-		if l <= heapSize && a[l-1] > a[k-1] {
+		if l <= heapSize && a[l] > a[k] {
 			largest = l
 		} else {
 			largest = k
 		}
-		if r <= heapSize && a[r-1] > a[largest-1] {
+		if r <= heapSize && a[r] > a[largest] {
 			largest = r
 		}
 		// if largest is left or right we swap values
 		if largest != k {
 			// complexity 0(1)
-			a[k-1], a[largest-1] = a[largest-1], a[k-1]
+			a[k-1], a[largest] = a[largest], a[k]
 			// to support property of non-increasing pyramid
 		}
 	}
@@ -71,6 +96,14 @@ func BuildMaxHeap(a []int, length int) {
 	}
 }
 
+func BuildMinHeap(a []int, length int) {
+	// we start building a pyramid from the tree leaves
+	// which each of them is pyramid with one element
+	for i := length / 2; i >= 0; i-- {
+		MinHeapifyRecursive(a, i, length)
+	}
+}
+
 // MaxHeapInsert  O(log n)
 func MaxHeapInsert(a []int, key int) ([]int, error) {
 	return HeapIncreaseKey(a, len(a), key)
@@ -81,11 +114,23 @@ func HeapExtractMax(a []int) int {
 	if len(a) == 0 {
 		return 0
 	}
-	aLen := len(a) - 1
+	length := len(a) - 1
 	max := a[0]
-	a[0] = a[aLen]
-	MaxHeapify(a, 1, aLen)
+	a = a[:length+copy(a[length:], a[length+1:])]
+	MaxHeapifyRecursive(a, 0, length)
 	return max
+}
+
+// HeapExtractMin O(log n)
+func HeapExtractMin(a *[]int) int {
+	if len(*a) == 0 {
+		return 0
+	}
+	length := len(*a) - 1
+	min := (*a)[0]
+	*a = (*a)[:0+copy((*a)[0:], (*a)[0+1:])]
+	MinHeapifyRecursive(*a, 0, length)
+	return min
 }
 
 // HeapIncreaseKey O(log n)
@@ -105,6 +150,23 @@ func HeapIncreaseKey(a []int, i, k int) ([]int, error) {
 	return a, nil
 }
 
+// HeapDecreaseKey O(log n)
+func HeapDecreaseKey(a []int, i, k int) ([]int, error) {
+	if i >= len(a) {
+		a = append(a, k)
+	} else {
+		if k > a[i] {
+			return nil, errors.New("new key more than current")
+		}
+		a[i] = k
+	}
+	for i > 1 && a[parent(i)] < a[i] {
+		a[i], a[parent(i)] = a[parent(i)], a[i]
+		i = parent(i)
+	}
+	return a, nil
+}
+
 // HeapSort
 // Complexity O(n log n)
 func HeapSort(a []int) {
@@ -118,5 +180,19 @@ func HeapSort(a []int) {
 		length--
 		// recovery a property of non-increasing pyramid
 		MaxHeapifyRecursive(a, 0, length)
+	}
+}
+
+func HeapSortDesc(a []int) {
+	length := len(a) - 1
+	BuildMinHeap(a, length)
+	for i := length; i > 0; i-- {
+		// The largest element (A[0]) is in the top of the pyramid
+		// So we swap it with the smallest (A[n]) element
+		a[0], a[i] = a[i], a[0]
+		// we decrement heap size
+		length--
+		// recovery a property of non-increasing pyramid
+		MinHeapifyRecursive(a, 0, length)
 	}
 }
